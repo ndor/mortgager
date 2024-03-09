cc0fe834e609fa6bcbbea5b99973ef7a57a7d401
Natanel <ndor123@gmail.com>
2024-03-09 03:45:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# import numpy as np
+import numpy as np
 from pyscript import document
 
 
 def translate_english(event):
-    # input_text = document.querySelector("#button1")
-    # english = input_text.value
-    # output_div = document.querySelector("#output")
-    # output_div.innerText = '50% מס'
-    pass
+    input_text = document.querySelector("#button1")
+    english = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = "50% מס"
